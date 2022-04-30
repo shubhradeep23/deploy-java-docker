a0bae676089124ce0a0964943694e321ab9b8309
shubhradeep23 <devops.april.2022@gmail.com>
2022-04-30 10:32:51 +0530

    testing docker image & run

--- src/main/java/com/example/k8s/springbootkubernetes/controller/WelcomeController.java
+++ src/main/java/com/example/k8s/springbootkubernetes/controller/WelcomeController.java
@@ -8,7 +8,7 @@ public class WelcomeController {
 	
 	@RequestMapping("/greeting")
 	public String greeting() {
-		return "Welcome to Shubhradeep's Channel";
+		return "Congratulations on building your first successful jenkins pipeline";
 	}
 
 }
